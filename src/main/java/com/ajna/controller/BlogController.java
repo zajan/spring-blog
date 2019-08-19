b4d26a639b64c382ab572b6b74b2ed88293cece7
zajan <aniaaa94@gmail.com>
2019-08-19 19:55:07 +0200

    add adding new posts feature

--- src/main/java/com/ajna/controller/BlogController.java
+++ src/main/java/com/ajna/controller/BlogController.java
@@ -1,10 +1,14 @@
 package com.ajna.controller;
 
+import com.ajna.model.Post;
+import com.ajna.service.CustomUserDetailsService;
 import com.ajna.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -14,6 +18,9 @@ public class BlogController {
 
     @Autowired
     PostService postService;
+
+    @Autowired
+    CustomUserDetailsService userService;
 
     @GetMapping("/posts")
     public String posts(Model model, Principal principal, HttpServletRequest request) {
@@ -31,4 +38,21 @@ public class BlogController {
         model.addAttribute("posts", postService.findAll(page, size));
         return "posts";
     }
+
+    @GetMapping("/new-post")
+    public String newPost(Model model) {
+        Post post = new Post();
+        model.addAttribute(post);
+        return "new-post-form";
+    }
+
+    @PostMapping("/save-post")
+    public String savePost(@ModelAttribute("post") Post post, Principal principal) {
+        if (principal != null) {
+            post.setUser(userService.findByUsername(principal.getName()));
+        }
+        postService.save(post);
+        return "redirect:/posts";
+    }
+
 }
